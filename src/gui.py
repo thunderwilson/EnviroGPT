814f35a78c5de75e3aea9f7389fe6f6a6b72ac69
thunderwilson <thomas.wilson983@gmail.com>
2023-05-11 23:45:11 +0000

    Instructions

--- src/gui.py
+++ src/gui.py
@@ -310,6 +310,7 @@ def output_add(q,a):
 # LAYOUT
 
 st.title('Why search when you can get _:green[answers]_?')
+st.subheader('Chat with reports, investigations, consents, regulations, plan rules...')
 
 
 with st.sidebar:
